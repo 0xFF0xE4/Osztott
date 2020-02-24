82481fc280229a43b46a627ad48073ab57b482b6
Zoitom <mihaelle98@gmail.com>
2020-02-24 17:05:31 +0100

    harmadik gyak eleje

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -8,16 +9,21 @@ public class Client {
     private static final String HOST = "localhost";
 
     public static void main(String[] args) throws IOException {
+        File inputFile = new File("src/input.txt");
         try (
                 Socket s = new Socket(HOST, PORT);
                 Scanner sc = new Scanner(s.getInputStream());
                 PrintWriter pw = new PrintWriter(s.getOutputStream());
+                Scanner fileReader = new Scanner(inputFile);
         ) {
-            pw.println(123);
-            pw.println(456);
-            pw.println(789);
-            pw.flush();
+            StringBuilder sb = new StringBuilder();
+            while(fileReader.hasNextInt()) {
+                pw.println(fileReader.nextInt());
+                pw.flush();
+                System.out.println(sc.nextInt());
+            }
 
         } catch (Exception e) { e.printStackTrace();}
     }
+
 }
--- src/Server1.java
+++ src/Server1.java
@@ -4,32 +4,38 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Server1 {
 
-    private static final int PORT = 10000;
+    private static int PORT = 10000;
+
+    /* 3. feladat innen: https://kitlei.web.elte.hu/segedanyagok/felev/2019-2020-tavasz/osztott/osztott-feladatok.html */
 
     public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+//            System.out.println("ERROR: port number not specified!");
+//            exit(1);
+            PORT = 10000;
+        } else {
+            PORT = Integer.parseInt(args[0]);
+        }
         try (
                 ServerSocket ss = new ServerSocket(PORT);
                 Socket s = ss.accept();
                 Scanner sc = new Scanner(s.getInputStream());
                 PrintWriter pw = new PrintWriter(s.getOutputStream());
         ) {
-            int sum = 0;
-            while (sc.hasNextInt()) {
-                int in = sc.nextInt();
-                if (in == 0) {
-                    pw.println(sum);
-                    pw.flush();
-                    sum = 0;
-                } else {
-                    sum += in;
-                }
+            while(sc.hasNextInt()) {
+                int n = sc.nextInt();
+                pw.println(transformInt(n));
+                pw.flush();
             }
-
         } catch (Exception e) { e.printStackTrace();}
+    }
 
-        // ss.close();
+    private static int transformInt(int n) {
+        return 2 * n + 1;
     }
 
 }
